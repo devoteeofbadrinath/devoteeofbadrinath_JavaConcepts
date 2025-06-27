795d6fe72212ee99e0624f65e047f19ff03f9376
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-06-27 16:07:14 +0530

    Message

---
+++ src/javaconcurrency/lostupdate/LostUpdateDemo.java
@@ -0,0 +1,43 @@
+package javaconcurrency.lostupdate;
+
+public class LostUpdateDemo {
+
+    private static class SynchronizedThread implements Runnable {
+        private int value = 0;
+
+        @Override
+        public void run() {
+
+            for (int i = 1; i < 5000; i++) {
+                increment();
+                Thread.yield();
+            }
+
+        }
+
+        public void increment() {
+            int i = value;
+            value = i + 1;
+        }
+
+        public int getValue() { return value;}
+    }
+
+    public static void main(String[] args) {
+        SynchronizedThread job = new SynchronizedThread();
+        Thread t1 = new Thread(job, "t1");
+        Thread t2 = new Thread(job, "t2");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(job.getValue());
+    }
+}
