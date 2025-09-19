5d3c89232b45a833fe7f0fe2ccc45794cfc9dff9
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-09-19 16:21:23 +0530

    Message

--- TAPP101772_CRM_Inv_Acc_Pub/src/main/java/com/fil/crm/paas/controller/EmployeeController.java
+++ TAPP101772_CRM_Inv_Acc_Pub/src/main/java/com/fil/crm/paas/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import com.fil.crm.paas.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,12 @@ public class EmployeeController {
     @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "Shivam");
+        return "hello";  // resolves to /WEB-INF/jsp/hello.jsp
     }
 
 //    @GetMapping
