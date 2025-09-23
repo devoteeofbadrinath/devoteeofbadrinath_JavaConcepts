d7d72b4c3cd2c0631befccf7b2d43c83cdaac424
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-09-23 08:41:41 +0530

    Message

--- TAPP101772_CRM_Inv_Acc_Pub/src/main/java/com/fil/crm/paas/controller/JspController.java
+++ TAPP101772_CRM_Inv_Acc_Pub/src/main/java/com/fil/crm/paas/controller/JspController.java
@@ -1,28 +1,15 @@
 package com.fil.crm.paas.controller;
 
-import
-        com.fil.crm.paas.constants.JsonMappingConstants;
-import com.fil.crm.paas.model.Employee;
-import com.fil.crm.paas.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class JspController {
 
     @GetMapping("/hello")
     public String hello(Model model) {
-        model.addAttribute("name", "Shivam");
+        //model.addAttribute("name", "Shivam");
         return "hello";  // resolves to /WEB-INF/jsp/hello.jsp
     }
-
 }
--- TAPP101772_CRM_Inv_Acc_Pub/src/main/webapp/WEB-INF/jsp/hello.jsp
+++ TAPP101772_CRM_Inv_Acc_Pub/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -12,6 +12,6 @@
 </head>
 <body>
 <h2>Hello, JSP from Spring Boot!</h2>
-<p>User: ${name}</p>
+<%--<p>User: ${name}</p>--%>
 </body>
 </html>
